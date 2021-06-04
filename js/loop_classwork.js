ee303ee00136c748de4070dfb384459fff3bbd3d
Kathleen Weaver <kathleen@kweaver.org>
2021-06-04 10:02:43 -0500

    for loops

---
+++ js/loop_classwork.js
@@ -0,0 +1,17 @@
+// This is stuff I used to teach loops
+
+/* Set this up for testing, uncomment to test
+alert("I have this working!");
+*/
+
+var message = '';
+
+// var howmany = prompt("How many?");
+
+for (let index = 0; index <= 10; index++) {
+    message = message + "index is: " +index + "\n";
+}
+
+alert(message);
+
+alert(index);
