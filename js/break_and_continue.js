6613cee2457798dbe4f84186df6a2af8b96bc664
Kathleen Weaver <kathleen@kweaver.org>
2021-06-04 15:05:21 -0500

    completed break and continue exercise

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,1 +1,21 @@
-alert("Here is break and continue");
+'use strict';
+
+// alert("Here is break and continue");
+
+var oddNumber;
+
+while (true) {
+    oddNumber = parseInt(prompt("Enter an odd number between 1 and 50"));
+    if (oddNumber % 2 != 0) {
+        break;
+    }
+}
+
+for (let index = 0; index < 50; index++) {
+    if (index == oddNumber) {
+        continue
+    }
+    if (index %2 !=0) {
+        console.log("Here is an odd number: " + index);
+    }
+}
