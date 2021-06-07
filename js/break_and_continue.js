eec9ac48a9e984c6ed07a303b2206e0804e6eabd
Kathleen Weaver <kathleen@kweaver.org>
2021-06-07 10:14:59 -0500

    looked at loops again

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,7 +1,8 @@
 'use strict';
 
-// alert("Here is break and continue");
+//alert("Here is break and continue");
 
+/*
 var oddNumber;
 
 while (true) {
@@ -22,4 +23,30 @@ for (let index = 0; index < 50; index++) {
         continue;
     }
     console.log("Here is an odd number: " + index);
+}
+*/
+
+// in the loop
+
+let odd_Number = 0;
+
+// checking for valid input
+while (true) {
+    odd_Number = prompt("Enter a odd number between 1 and 50");
+    if ((odd_Number > 1) && (odd_Number <= 50)) {
+        if (odd_Number % 2 == 1) {
+            break;
+        }
+    }
+}
+
+console.log("Number to skip is: " + odd_Number);
+
+// outputing the required text
+for (let number = 1; number <50; number = number + 2) {
+    if (odd_Number == number) {
+        console.log("Yikes! Skipping number: " + odd_Number);
+        continue;
+    }
+    console.log("Here is an odd number: " + number);
 }
