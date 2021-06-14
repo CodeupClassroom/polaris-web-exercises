b8b1f6b8c03492de1c4a82ffbc22253cae092ed4
Kathleen Weaver <kathleen@kweaver.org>
2021-06-14 11:30:37 -0500

    done

--- js/conditionals-test.js
+++
@@ -1,9 +0,0 @@
-describe('calculateTotal', function() {
-    it('should be a defined function', function () {
-        expect(calculateTotal).toBeDefined();
-    });
-    it('should return undefined if no values passed', function () {
-       expect(typeof calculateTotal()).toBe("undefined");
-    });
-
-});
