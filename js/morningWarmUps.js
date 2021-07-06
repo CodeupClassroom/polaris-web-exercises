26090bd6b9791f891b9277dbfd2cdc41ce0eb8b3
Samuel Moore <samuel.c.moore1221@gmail.com>
2021-07-06 10:24:54 -0500

    finished DOM Events lecture

--- js/morningWarmUps.js
+++ js/morningWarmUps.js
@@ -17,3 +17,12 @@
         return input.split(" ").length - 1
 
     }
+
+// July 6th 2021
+
+// TODO: Create a function named capitalizeAllNames that accepts an array of strings and returns the array with all of its elements capitalized.
+// Example:  capitalizeAllNames(["bob","Seth","TOFU","Toyota"]) ---- returns ----> ["BOB","SETH","TOFU","TOYOTA"]
+
+// TODO: Create a function named capitalizeFirstLetter that accepts an array of strings and returns the array with all of the first letters in
+//  each string being capitalized while having the rest of the string lowercase look at the example below for reference.
+// Example:  capitalizeFirstLetter(["bob","Seth","TOFU","Toyota"]) ---- returns ----> ["Bob","Seth","Tofu","Toyota"]
