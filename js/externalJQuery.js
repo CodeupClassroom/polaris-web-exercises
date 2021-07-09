ef29a0171ed35fdf7dcfa5bf8d5ea4e2e3693757
Samuel Moore <samuel.c.moore1221@gmail.com>
2021-07-09 12:28:34 -0500

    pushing up jquery selectors lecture content

--- js/externalJQuery.js
+++ js/externalJQuery.js
@@ -1,10 +1,40 @@
 "use strict"
+var h1;
+var aDiv
 $(document).ready(function (){
 
-    var h1 = $("h1")
+    h1 = $("h1")
 
-    console.log(h1)
+    console.log(h1.html("Sam was here"))
 
     alert("hey I am ready for manipulation!")
+
+
+    aDiv = $("#something")
+
+    console.log(aDiv)
+
+    var divs = $(".content")
+
+    console.log(divs)
+
+    divs.addClass("imgStuff")
+
+    var stuff = $(".content, h1")
+
+    console.log(stuff)
+
+    var image = $("img")
+
+    // image.css("width","50%")
+    // image.css("border","solid 5px red")
+
+    image.css({"width":"50%","border":"solid 5px red"})
+
+    console.log("The images width is " + image.css("width"))
+
+    // console.log(image.css())
+
+    $("div > p,h1").css({"background-color":"cyan"})
 
 })
